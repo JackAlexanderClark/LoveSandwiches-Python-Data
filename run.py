cf3fd3b5d4bb07d87f307d9d3ccdf0be6c63f0de
JackAlexanderClark <jackalexanderclark@protonmail.com>
2022-05-29 10:37:59 +0000

    1

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 """
 Checking our API works
-Calling sales worksheet from our google spread sheet. 
+Calling sales worksheet from our google spread sheet
 Data variable to print sales data to the terminal using: python3 run.py
 """
 sales = SHEET.worksheet('sales')
@@ -34,9 +34,28 @@ def get_sales_data():
     """
     print("Please enter sales data from the last market.")
     print("Data should be six numbers, seperated by commas, CSV format.")
-    print("Example: 10,20,30,40,50,60.")
+    print("Example: 10,20,30,40,50,60.\n")
 
-    data_str = input("Enter your data here: ")
-    print(f"The data provided is: {data_str}")
+    data_string = input("Enter your data here: ")
+
+    #Converting string by split() method returns the broken up values as list
+    sales_data = data_string.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        if len(values) != 6:
+            raise ValueError(f"We expected to receive 6 data values, you inputted {len(values)}")
+    #as keyword assigns assigns the ValueError to e (shorthand error)
+    except ValueError as e:
+        print(f"Invalid data {e}, please try again.\n")
+
 
 get_sales_data()
+
+
